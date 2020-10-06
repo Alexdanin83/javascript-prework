1ae7da555fe1fcefe9f60eecc2702f5a53dc0c6f
Alex Danin <alexey.danin@gmail.com>
2020-10-06 21:05:57 +0200

    Modul 6.1

--- js/functions.js
+++ js/functions.js
@@ -1,55 +1,56 @@
-function printMessage(msg){
+{
+const printMessage = function(msg){
 	let div = document.createElement('div');
 	div.innerHTML = msg;
 	document.getElementById('messages').appendChild(div);
 }
-function printMessageRound(msg){
+const printMessageRound = function(msg){
 	let div = document.createElement('div');
 	div.innerHTML = msg;
 	document.getElementById('round').appendChild(div);
 }
-function clearMessages(){
+const clearMessages = function(){
 	document.getElementById('messages').innerHTML = '';
 	document.getElementById('round').innerHTML = '';
 }
-function playGame(playerInput){
+const playGame = function(playerInput){
 	clearMessages();
 	console.log(numberComputer*100/(numberPlayer+numberComputer))
 	let playerInputMemory=playerInput;
 	let randomOutput = Math.floor(Math.random() * 3 + 1);
 	if(randomOutput == 1){
-    randomOutput= 'kamień';
-  }
-  if(randomOutput == 2){
-    randomOutput= 'papier';
-  }
-  if(randomOutput == 3){
-    randomOutput= 'nożyce';
-  }
-  if((randomOutput == 'kamień' && playerInput == 'papier')
-  ||(randomOutput == 'nożyce' && playerInput == 'kamień')
-  ||(randomOutput == 'papier' && playerInput == 'nożyce')){
-		numberPlayer++;
-    printMessage('Ty wygrywasz!');
+     randomOutput= 'kamień';
+  	}
+  	if(randomOutput == 2){
+	randomOutput= 'papier';
+  	}
+  	if(randomOutput == 3){
+    	randomOutput= 'nożyce';
+  	}
+  	if((randomOutput == 'kamień' && playerInput == 'papier')
+  	||(randomOutput == 'nożyce' && playerInput == 'kamień')
+  	||(randomOutput == 'papier' && playerInput == 'nożyce')){
+	numberPlayer++;
+     printMessage('Ty wygrywasz!');
+	printMessage('Zagrałem ' + randomOutput + ', a Ty ' + playerInput);
+	printMessageRound("Ty wygrałeś: "+numberPlayer+" Remis: "+remis+" Wygrał computer:"+numberComputer)
+  	} else
+ 	if(randomOutput == playerInput ){
+	remis++;
+	printMessage('Remis');
+	printMessage('Zagrałem ' + randomOutput + ', a Ty ' + playerInput);
+	printMessageRound("Ty wygrałeś: "+numberPlayer+" Remis: "+remis+" Wygrał computer:"+numberComputer)
+ 	}
+ 	else {
+		if (((numberComputer+1)*100/(numberPlayer+numberComputer))<25) {
+		numberComputer++;
+    		printMessage('Tym razem przegrywasz :(');
 		printMessage('Zagrałem ' + randomOutput + ', a Ty ' + playerInput);
 		printMessageRound("Ty wygrałeś: "+numberPlayer+" Remis: "+remis+" Wygrał computer:"+numberComputer)
-  } else
-  if(randomOutput == playerInput ){
-		remis++;
-		printMessage('Remis');
-		printMessage('Zagrałem ' + randomOutput + ', a Ty ' + playerInput);
-		printMessageRound("Ty wygrałeś: "+numberPlayer+" Remis: "+remis+" Wygrał computer:"+numberComputer)
-  }
-  else {
-				if (((numberComputer+1)*100/(numberPlayer+numberComputer))<25) {
-					numberComputer++;
-    			printMessage('Tym razem przegrywasz :(');
-					printMessage('Zagrałem ' + randomOutput + ', a Ty ' + playerInput);
-					printMessageRound("Ty wygrałeś: "+numberPlayer+" Remis: "+remis+" Wygrał computer:"+numberComputer)
-    	}
-		 	else {
-					playGame(playerInputMemory)
-		 		}
+    		}
+		else {
+		playGame(playerInputMemory)
+		}
     }
 }
 
@@ -59,3 +60,4 @@ let remis=0;
 document.getElementById('play-rock').addEventListener('click', function(){playGame('kamień')});
 document.getElementById('play-paper').addEventListener('click', function(){playGame('papier')});
 document.getElementById('play-scissors').addEventListener('click', function(){playGame('nożyce')});
+}
